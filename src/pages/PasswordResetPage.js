38d7b9310722650bb360285ccc908d1b19afad39
shulin li <1261341209@qq.com>
2024-03-23 16:35:52 +1100

    修改了password reset

--- src/pages/PasswordResetPage.js
+++ src/pages/PasswordResetPage.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, TextField, Button, Typography } from "@mui/material";
-
+import { Link } from "react-router-dom";
 const PasswordResetPage = () => {
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [email, setEmail] = useState("");
+  // 用于检测email是否符合格式
+  const [emailError, setEmailError] = useState("");
+  // confirm password
+  const [pwdCon, setpwdCon] = useState("");
+  // 用于检测confirm password是否为空
+  const [pwdConError, setPwdConError] = useState("");
+  const [code, setCode] = useState("");
+  // 用于检测code是否为空
+  const [codeError, setCodeError] = useState("");
+  // 控制发送code按钮60s内不能按
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+    if (!event.target.value.trim()) {
+      // 不符合要求则报错
+      setCodeError("Code cannot be empty");
+    } else {
+      setCodeError("");
+    }
+  };
+  // 监控发送code按钮，按下后60s无法再按
+  useEffect(() => {
+    let timer;
+    if (buttonDisabled) {
+      timer = setTimeout(() => setButtonDisabled(false), 60000);
+    }
+    return () => clearTimeout(timer);
+  }, [buttonDisabled]);
+  const getCode = () => {
+    // 只对符合格式要求的发
+    if (!validateEmail(email)) {
+      setEmailError("Please enter a valid email address");
+      return;
+    } else {
+      setEmailError("");
+    }
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    if (!event.target.value.trim()) {
+      // 不符合要求则报错
+      setPasswordError("Password cannot be empty");
+    } else {
+      setPasswordError("");
+    }
+  };
+  const validateEmail = (email) => {
+    const regexPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexPattern.test(email);
+  };
+  // email变动时检测是否符合要求
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    if (!validateEmail(event.target.value)) {
+      // 不符合格式报错
+      setEmailError("Please enter a valid email address");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const handlePwdConChange = (event) => {
+    setpwdCon(event.target.value);
+    if (!event.target.value.trim()) {
+      // 不符合要求则报错
+      setPwdConError("Confirm Password cannot be empty");
+    } else {
+      setPwdConError("");
+    }
+    if (password !== event.target.value) {
+      // 与password不相同报错
+      setPwdConError("Confirm Password must be the same as password");
+    } else {
+      setPwdConError("");
+    }
+  };
   return (
     <Box
       sx={{
@@ -9,7 +88,7 @@ const PasswordResetPage = () => {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        height: "100vh",
+        mt: 5,
       }}
     >
       <Typography variant="h4" sx={{ mb: 2 }}>
@@ -17,13 +96,93 @@ const PasswordResetPage = () => {
       </Typography>
       <TextField
         label="Email Address"
+        onChange={handleEmailChange}
         variant="outlined"
-        type="email"
         sx={{ mb: 2, width: "300px" }}
       />
-      <Button variant="contained" sx={{ width: "300px" }}>
-        Send Reset Link
-      </Button>
+      {emailError && (
+        <Typography
+          sx={{
+            color: "error.main", // 使用主题中的错误颜色
+            fontSize: "0.75rem", // 相当于12px
+          }}
+        >
+          {emailError}
+        </Typography>
+      )}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center", // 垂直居中
+          gap: "20px", // 子元素之间的间隔设置为20px
+          width: "300px", // 总宽度设置为300px
+          mb: 2,
+        }}
+      >
+        <TextField
+          label="verification code"
+          variant="outlined"
+          onChange={handleCodeChange}
+          sx={{ flexBasis: "220px" }} // 设置基础宽度
+        />
+        <Button
+          onClick={getCode}
+          disabled={buttonDisabled}
+          variant="contained"
+          sx={{ flexBasis: "60px" }} // 设置基础宽度
+        >
+          code
+        </Button>
+      </Box>
+      {codeError && (
+        <Typography
+          sx={{
+            color: "error.main", // 使用主题中的错误颜色
+            fontSize: "0.75rem", // 相当于12px
+          }}
+        >
+          {codeError}
+        </Typography>
+      )}
+      <TextField
+        label="Password"
+        onChange={handlePasswordChange}
+        variant="outlined"
+        type="password"
+        sx={{ mb: 2, width: "300px" }}
+      />
+      {passwordError && (
+        <Typography
+          sx={{
+            color: "error.main", // 使用主题中的错误颜色
+            fontSize: "0.75rem", // 相当于12px
+          }}
+        >
+          {passwordError}
+        </Typography>
+      )}
+      <TextField
+        label="Password confirmation"
+        onChange={handlePwdConChange}
+        variant="outlined"
+        type="password"
+        sx={{ mb: 2, width: "300px" }}
+      />
+      {pwdConError && (
+        <Typography
+          sx={{
+            color: "error.main", // 使用主题中的错误颜色
+            fontSize: "0.75rem", // 相当于12px
+          }}
+        >
+          {pwdConError}
+        </Typography>
+      )}
+      <Link to="/login">
+        <Button variant="contained" sx={{ mb: 2, width: "300px" }}>
+          Reset
+        </Button>
+      </Link>
     </Box>
   );
 };
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,11 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
 import TextField from "@mui/material/TextField";
-
+import Typography from "@mui/material/Typography";
 const SignUpPage = () => {
   const [isOrganizer, setIsOrganizer] = useState(true);
+  const [username, setUsername] = useState("");
+  // 用于检测username是否为空
+  const [usernameError, setUsernameError] = useState("");
+  // email
+  const [email, setEmail] = useState("");
+  // 用于检测email是否符合格式
+  const [emailError, setEmailError] = useState("");
+
+  // password
+  const [password, setPassword] = useState("");
+  //phone number
+  const [phone, setphone] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  // 用于检测password是否为空
+  const [passwordError, setPasswordError] = useState("");
+  // confirm password
+  const [pwdCon, setpwdCon] = useState("");
+  // 用于检测confirm password是否为空
+  const [pwdConError, setPwdConError] = useState("");
+  // username变动时检测是否符合要求
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+    if (!event.target.value.trim()) {
+      // 不符合要求则报错
+      setUsernameError("Username cannot be empty");
+    } else {
+      setUsernameError("");
+    }
+  };
+
+  // 邮件正则
+  const validateEmail = (email) => {
+    const regexPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexPattern.test(email);
+  };
+  // email变动时检测是否符合要求
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    if (!validateEmail(event.target.value)) {
+      // 不符合格式报错
+      setEmailError("Please enter a valid email address");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    if (!event.target.value.trim()) {
+      // 不符合要求则报错
+      setPasswordError("Password cannot be empty");
+    } else {
+      setPasswordError("");
+    }
+  };
+  const handlePhoneChange = (event) => {
+    setphone(event.target.value);
+    if (!event.target.value.trim()) {
+      setPhoneError("phone number cannot be empty");
+    } else {
+      setPhoneError("");
+    }
+  };
+  // cofirm password变动时检测是否符合要求
+  const handlePwdConChange = (event) => {
+    setpwdCon(event.target.value);
+    if (!event.target.value.trim()) {
+      // 不符合要求则报错
+      setPwdConError("Confirm Password cannot be empty");
+    } else {
+      setPwdConError("");
+    }
+    if (password !== event.target.value) {
+      // 与password不相同报错
+      setPwdConError("Confirm Password must be the same as password");
+    } else {
+      setPwdConError("");
+    }
+  };
 
   return (
     <Box sx={{ display: "flex", justifyContent: "center", mt: 4, gap: 4 }}>
@@ -62,25 +141,91 @@ const SignUpPage = () => {
               variant="outlined"
               sx={{ mb: 2 }}
             />
+            <TextField
+              label="Name"
+              onChange={handleUsernameChange}
+              variant="outlined"
+              sx={{ mb: 2 }}
+            />
+            {usernameError && (
+              <Typography
+                sx={{
+                  color: "error.main", // 使用主题中的错误颜色
+                  fontSize: "0.75rem", // 相当于12px
+                }}
+              >
+                {usernameError}
+              </Typography>
+            )}
           </>
         )}
 
-        <TextField label="Name" variant="outlined" sx={{ mb: 2 }} />
-        <TextField label="Email Address" variant="outlined" sx={{ mb: 2 }} />
+        <TextField
+          label="Email Address"
+          onChange={handleEmailChange}
+          variant="outlined"
+          sx={{ mb: 2 }}
+        />
+        {emailError && (
+          <Typography
+            sx={{
+              color: "error.main", // 使用主题中的错误颜色
+              fontSize: "0.75rem", // 相当于12px
+            }}
+          >
+            {emailError}
+          </Typography>
+        )}
         <TextField
           label="Password"
+          onChange={handlePasswordChange}
           variant="outlined"
           type="password"
           sx={{ mb: 2 }}
         />
+        {passwordError && (
+          <Typography
+            sx={{
+              color: "error.main", // 使用主题中的错误颜色
+              fontSize: "0.75rem", // 相当于12px
+            }}
+          >
+            {passwordError}
+          </Typography>
+        )}
         <TextField
           label="Password confirmation"
+          onChange={handlePwdConChange}
           variant="outlined"
           type="password"
           sx={{ mb: 2 }}
         />
-        <TextField label="Phone Number" variant="outlined" sx={{ mb: 2 }} />
-
+        {pwdConError && (
+          <Typography
+            sx={{
+              color: "error.main", // 使用主题中的错误颜色
+              fontSize: "0.75rem", // 相当于12px
+            }}
+          >
+            {pwdConError}
+          </Typography>
+        )}
+        <TextField
+          label="Phone Number"
+          onChange={handlePhoneChange}
+          variant="outlined"
+          sx={{ mb: 2 }}
+        />
+        {phoneError && (
+          <Typography
+            sx={{
+              color: "error.main", // 使用主题中的错误颜色
+              fontSize: "0.75rem", // 相当于12px
+            }}
+          >
+            {phoneError}
+          </Typography>
+        )}
         <Link to={isOrganizer ? "/Org_Acc" : "/Cus_Acc"}>
           <Button variant="contained" sx={{ mb: 2 }}>
             Sign Up
