a186056a98cf4737211871f64cf4073a77f34ec2
default <yongshi0627@gmail.com>
2024-04-17 15:06:19 +1000

    update contact

--- myproj/frontend/src/pages/ContactPage.js
+++ myproj/frontend/src/pages/ContactPage.js
@@ -11,10 +11,18 @@ const ContactPage = () => {
                 height: "100vh",
             }}
         >
-            <h1>
-                ContactPage
-            </h1>
+            <h2>
+                Our team Information      
+            </h2>
+            <div>
+            <h3>Liyongshi Chen</h3>
+            <h3>Zhuoyang Li</h3>
+            <h3>Huihua Huang</h3>
+            <h3>Shulin Li</h3>
+            <h3>Jinpeng Ni</h3>
+            </div>
         </div>
+        
     );
 };
  
